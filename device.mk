1ca6b959a164ada31daae109729a1924c9af21f3
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-02-13 11:16:02 +0800

    deb: Bring up CM
     * Add proprietary file scripts
     * Add widevine blobs and remove unnecessary blobs
     * Configure hardware keys appropriately
     * Move over to Trebuchet from Launcher2
     * Don't restrict vendor files
    
    Change-Id: Iae45847deb780fbf3b6a118bc4c541c45f5c675c

--- device.mk
+++ device.mk
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+$(call inherit-product-if-exists, vendor/asus/deb/deb-vendor.mk)
 
 # rild
 PRODUCT_PACKAGES := \
