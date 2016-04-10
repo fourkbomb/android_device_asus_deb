fb492eeec2267735e113e449f13d6664ba126424
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-04-10 15:28:01 +0800

    deb: Inherit CM telephony stuff
    Change-Id: I9bf5b9b507c77fbb5e6089ec36a260c335624333

--- cm.mk
+++ cm.mk
@@ -15,6 +15,9 @@
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
+# Inherit telephony stuff
+$(call inherit-product, vendor/cm/config/telephony.mk)
+
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
